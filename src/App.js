b1ed6d9a640d211e32dc3d83b782b3df364aa9cc
Mohamed EL Moumny <mohamed.elmoumny-etu@etu.univh2c.ma>
2022-02-18 18:25:08 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,245 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import useSpeechSynthesis from "./hooks/useSpeechSynthesis";
+import {
+    englishQuestionsTopic1,
+    englishQuestionsTopic2,
+    chineseQuestionsTopic1,
+    chineseQuestionsTopic2,
+    englishQuestionsTopic3,
+    chineseQuestionsTopic3,
+} from "./data/testData";
+
+const utterance = new window.SpeechSynthesisUtterance();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const say = (str) => {
+        window.speechSynthesis.cancel();
+        utterance.text = str;
+        utterance.voice = speechSynthesis.getVoices()[voice === 1 ? 4 : 19];
+        utterance.rate = 0.9;
+        utterance.pitch = 1;
+        utterance.volume = 1;
+        window.speechSynthesis.speak(utterance);
+    };
+    // * get SpeechSynthesis variables
+    const { speak, voices, speaking } = useSpeechSynthesis({ onEnd: () => console.log("is end") });
+    // const text = welcomeData[1];
+    // const voice = voices[4];
+
+    // * question number State
+    // const [questionNumber, setQuestionNumber] = useState(0);
+    //   //* start listening
+    //   useEffect(() => {
+    //     console.log(speaking + ">>>>");
+    //     say("你好", 19);
+    // }, [speaking]);
+
+    const [questionNumber, setQuestionNumber] = useState(0);
+    const [beHappy, setBeHappy] = useState(true);
+    const [voice, setVoice] = useState(1);
+
+    // * controll the topics
+    const [topicNumber, setTopicNumber] = useState(1);
+    const topicController = () => {
+        console.log("topic number ===>> " + topicNumber);
+        setQuestionNumber(0);
+        if (topicNumber <= 3) {
+            setTopicNumber((topicNumber) => topicNumber + 1);
+        } else {
+            setTopicNumber(0);
+        }
+    };
+    // * get the topic number
+    const getTopicNumber = (topicNumber, arrOfSolutions) => {
+        console.log("topic number ===>> " + topicNumber);
+        if (topicNumber === 1) {
+            return arrOfSolutions[0];
+        } else if (topicNumber === 2) {
+            return arrOfSolutions[1];
+        } else {
+            return arrOfSolutions[2];
+        }
+    };
+
+    // ? function for check the questions length
+    const checker = (questions, startingWord) => {
+        console.log(" length ===>> " + questions.length);
+        if (questionNumber < questions.length) {
+            if (beHappy) {
+                // * choosing "be happy" at the first time
+                console.log(questionNumber + " ===>> " + questions[questionNumber]);
+                say({ startingWord } + questions[questionNumber]);
+                setBeHappy(false);
+                setQuestionNumber((questionNumber) => questionNumber + 1);
+            } else {
+                console.log(questionNumber + " ===>> " + questions[questionNumber]);
+                say(questions[questionNumber]);
+                setQuestionNumber((questionNumber) => questionNumber + 1);
+            }
+        } else {
+            console.log(">>>> the end of questions");
+            say(
+                "Through the above questions, do you still want to continue dating? Please answer Yes or No",
+                5
+            );
+            topicController();
+        }
+    };
+    // * listening commands
+    const commands = [
+        {
+            command: ["hi", "hello", "hey", "你好"], // ? the bot will be starting with english langue or chinese langue
+            callback: ({ command }) => {
+                console.log(command + " >>>");
+                if (["hi", "hello", "hey"].includes(command)) {
+                    //? detect hi , hello or hey
+                    say(
+                        "Hi, I am your personal AI love coach behappy I will accompany you during dating, don't be nervous Take a deep breath,"
+                    );
+                    setTimeout(() => {
+                        say(
+                            "I'll ask you both a lot of interesting questions Just answering alternatively Remember to look the eyes each other when talking Because the eyes are the windows to the soul."
+                        );
+                        setTimeout(
+                            () =>
+                                say(
+                                    " When you both finish answer, please say “BeHappy” to let me know"
+                                ),
+                            9600
+                        );
+                    }, 6200);
+                    setVoice(1);
+                } else {
+                    // ? detect chinese hello
+                    say(
+                        `您好，我是你专属的人工智能爱情教练 小悦
+                    接下来，我将陪伴你一起约会，不要紧张喔
+                     放松深呼吸，我会问你们俩很多有趣的问题
+                     你们轮流回答就好了
+                    记得说话的时候，要看着彼此的眼睛
+                    因为眼睛是心灵的窗
+                    当你们俩都回答完，请对我说 “小悦”`
+                    );
+                    say(
+                        "您好，我是你专属的人工智能爱情教练, 小悦 接下来，我将陪伴你一起约会，不要紧张喔, 放松深呼吸"
+                    );
+                    setTimeout(() => {
+                        say(
+                            "我会问你们俩很多有趣的问题,你们轮流回答就好了, 记得说话的时候，要看着彼此的眼睛 因为眼睛是心灵的窗口"
+                        );
+                        setTimeout(() => say("当你们俩都回答完，请对我说 “小悦”."), 9600);
+                    }, 7600);
+                    setVoice(0);
+                }
+            },
+        },
+        {
+            command: ["yes", "no", "是", "否"],
+            callback: ({ command }) => {
+                if (command === "yes") {
+                    getTopicNumber(topicNumber, [
+                        say(
+                            "Great, it looks like you have good conversation. Science shows that every great love story is a never-ending conversation! Next, I will keep asking you both more fun and interesting questions. waiting for you talking together, Be You, Be Happy!"
+                        ),
+                        say(
+                            "Great, it looks like you have good conversation. Science shows that every great love story is a never-ending conversation! Next, I will keep asking you both more fun and interesting questions. waiting for you talking together, Be You, Be Happy!"
+                        ),
+                        say(
+                            "Congratulations, you find your love successfully. Please check your dating prediction report in your Email! Today, all of questions has helped you know each other in the first impression. Science shows that every great love story is a never-ending conversation!  I have more dating coaching service to help you fall in love scientifically.  Waiting for you talking together and find your true love! Be You, Be Happy!"
+                        ),
+                    ]);
+                } else if (command === "是") {
+                    // ? bot detects "yes" in chinese langue
+                    getTopicNumber(topicNumber, [
+                        say(
+                            "非常棒，看起来你们很聊的来喔，科学表明，真爱就是一辈子都有聊不完的话题喔！接下来我会继续问你们，更多好玩有趣的问题，等着你们一起来聊，做最好的自己，一起必悦喔！"
+                        ),
+                        say(
+                            "非常棒，看起来你们很聊的来喔，科学表明，真爱就是一辈子都有聊不完的话题喔！接下来我会继续问你们，更多好玩有趣的问题，等着你们一起来聊，做最好的自己，一起必悦喔！"
+                        ),
+                        say(
+                            "恭喜你们，牵手成功!稍后你会在邮箱里收到你们的约会预测报告！今天的问题让你们彼此有了初步的了解，真爱需要科学经营，我还有更多的约会教练服务，帮助你们科学相爱，等着你们一起来聊，找到属于自己的真爱喔！做最好的自己，一起必悦喔！"
+                        ),
+                    ]);
+                } else if (command === "否") {
+                    // ? bot detects "no" in chinese langue
+                    getTopicNumber(topicNumber, [
+                        say(
+                            "不要难过，相爱是荷尔蒙的高度选择，这个世界总有一个属于你的真爱，在不远的地方等着你，我会继续帮助你们，科学找寻真爱喔！做最好的自己，一起必悦喔"
+                        ),
+                        say(
+                            "不要难过，相爱是荷尔蒙的高度选择，这个世界总有一个属于你的真爱，在不远的地方等着你，我会继续帮助你们，科学找寻真爱喔！做最好的自己，一起必悦喔"
+                        ),
+                        say(
+                            "不要难过，相爱是荷尔蒙的高度选择，这个世界总有一个属于你的真爱，在不远的地方等着你. 稍后你会在邮箱里收到你们的约会预测报告. 我会继续帮助你们，科学找寻真爱喔！做最好的自己，一起必悦喔！"
+                        ),
+                    ]);
+                } else {
+                    getTopicNumber(topicNumber, [
+                        say(
+                            "Don't be sad, love is a high choice of hormones. You deserve for true love, who is waiting for you in this world.  I will continue to help you to find your true love scientifically! Be You, Be Happy!"
+                        ),
+                        say(
+                            "Don't be sad, love is a high choice of hormones. You deserve for true love, who is waiting for you in this world.  I will continue to help you to find your true love scientifically! Be You, Be Happy!"
+                        ),
+                        say(
+                            "Don't be sad, love is a high choice of hormones. You deserve for true love, who is waiting for you in this world.  Please check your dating prediction report in your Email .I will continue to help you to find your true love scientifically! Be You, Be Happy!"
+                        ),
+                    ]);
+                }
+            },
+        },
+        {
+            command: ["be happy", "谢谢"],
+            callback: ({ command }) => {
+                console.log(command + " >>>");
+                console.log("question ==>> " + questionNumber); // ? number of question
+                if (command === "be happy") {
+                    // * english progress
+                    // getTopicNumber(topicNumber, [
+                    //     checker(englishQuestionsTopic1, "so let's start the Questions ...."),
+                    //     checker(englishQuestionsTopic2, "so let's start the Questions ...."),
+                    //     checker(englishQuestionsTopic3, "so let's start the Questions ...."),
+                    // ]);
+                    console.log("topic number ===>> " + topicNumber);
+                    if (topicNumber === 1) {
+                        checker(englishQuestionsTopic1, "so let's start the Questions ....");
+                    } else if (topicNumber === 2) {
+                        checker(englishQuestionsTopic2, "so let's start the Questions ....");
+                    } else {
+                        checker(englishQuestionsTopic3, "so let's start the Questions ....");
+                    }
+                } else {
+                    // * chinese progress
+                    getTopicNumber(topicNumber, [
+                        checker(chineseQuestionsTopic1, "所以讓我們開始問題 ...."),
+                        checker(chineseQuestionsTopic2, "所以讓我們開始問題 ...."),
+                        checker(chineseQuestionsTopic3, "所以讓我們開始問題 ...."),
+                    ]);
+                }
+            },
+        },
+    ];
+
+    // * get speechRecognition variables
+    const { transcript, resetTranscript, browserSupportsSpeechRecognition, listening } =
+        useSpeechRecognition({ commands });
+
+    useEffect(() => SpeechRecognition.startListening({ language: "zh-CN" }));
+
+    // *  check if browser supports or not
+    if (!browserSupportsSpeechRecognition) {
+        console.log("not supported");
+    }
+
+    // useEffect(() => console.log(transcript), [transcript]);
+
+    return (
+        <div className="">
+            <img className="pics" src="/img/2.jpg" alt="" />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data/testData.js
@@ -0,0 +1,98 @@
+const englishQuestionsTopic1 = [
+    "Given the choice of anyone in the world, whom would you want as a dinner guest?",
+    "Would you like to be famous? In what way?",
+    "Before making a telephone call, do you ever rehearse what you are going to say?",
+    // "Why?",
+    // "What would constitute a “perfect” day for you?",
+    // "When did you last sing to yourself? To someone else?",
+    // "If you were able to live to the age of 90 and retain either the mind or body of a 30-year-old for the last 60 years of your life, which would you want?",
+    // "Do you have a secret hunch about how you will die?",
+    // "Name three things you and your partner appear to have in common.",
+    // "For what in your life do you feel most grateful?",
+    // "If you could change anything about the way you were raised, what would it be?",
+];
+
+const englishQuestionsTopic2 = [
+    "If a crystal ball could tell you the truth about yourself, your life, the future or anything else, what would you want to know?",
+    "Is there something that you’ve dreamed of doing for a long time? Why haven’t you done it?",
+    "What is the greatest accomplishment of your life? ",
+    // "What do you value most in a friendship? ",
+    // "What is your most treasured memory? ",
+    // "What is your most terrible memory? ",
+    // "If you knew that in one year you would die suddenly, would you change anything about the way you are now living?",
+    // "Why? ",
+    // "What does friendship mean to you?",
+    // "What roles do love and affection play in your life? ",
+    // "Alternate sharing something you consider a positive characteristic of your partner. Share a total of five items. How close and warm is your family? Do you feel your childhood was happier than most other people’s?",
+    // "How do you feel about your relationship with your mother?",
+];
+
+const englishQuestionsTopic3 = [
+    "Make three true “we” statements each. For instance, “We are both in this zoom room feeling ?",
+    " Complete this sentence: “I wish I had someone with whom I could share ... Bla, bla, bla “... “ ?",
+    "If you were going to become a close friend with your partner, please share what would be important for him or her to know ?",
+    // "Tell your partner what you like about them; be very honest this time, saying things that you might not say to someone you’ve just met ?",
+    // "Share with your partner an embarrassing moment in your life ? ",
+    // "When did you last cry in front of another person? By yourself ?",
+    // "Tell your partner something that you like about them already ?",
+    // "What, if anything, is too serious to be joked about? ",
+    // "If you were to die this evening with no opportunity to communicate with anyone, what would you most regret not having told someone? Why haven’t you told them yet?",
+    // "Your house, containing everything you own, catches fire. After saving your loved ones and pets, you have time to safely make a final dash to save any one item. What would it be? Why?",
+    // "Of all the people in your family, whose death would you find most disturbing? Why? ",
+    // "Share a personal problem and ask your partner’s advice on how he or she might handle it. Also, ask your partner to reflect back to you how you seem to be feeling about the problem you have chosen ?",
+    // "Before making final decision for dating, what’s your last question for your partner? When you both finish answer, please say ‘BEHAPPY’ ? ",
+];
+
+const chineseQuestionsTopic1 = [
+    "在世界上所有人中选择，你想邀请谁和你共进晚餐？",
+    "你想成名吗?",
+    "想以什么方式成名呢？",
+    "打电话之前你会先排练下，要说什么吗？为什么呢？",
+    "对你来说，“完美”的一天是什么样的？",
+    "你上次自己唱歌是什么时候?是为某个人唱那首歌吗？",
+    "假如你能活到90岁，你是希望在30岁以后，继续保持30岁的心智，还是保持30岁的身体呢?",
+    "你是否曾经有过神秘的预感,  自己以怎样的方式死去？",
+    "说出三件你和你的约会对象看起来的共同点。你人生中最感激的是什么？",
+    "如果你能改变你成长的方式，你会改变什么？",
+    "花四分钟，尽可能详细地告诉你的约会对象，你的人生故事.如果你明天一觉醒来，拥有了一种新的品质或能力，你希望那会是什么呢？",
+];
+
+const chineseQuestionsTopic2 = [
+    "假如有个魔力水晶球，可以告诉你们，关于你自己、你的人生、你的未来或者任何事情的真相，你会想知道什么？",
+    "有没有某件事是你一直梦想去做的但是到现在都还没有做的，为什么没有做呢？",
+    "你人生中最大的成就是什么？",
+    "友谊中，你最珍视的是什么？",
+    "你最珍贵的记忆是什么？",
+    "你最糟糕的记忆是什么？",
+    "如果你知道你只有一年的生命，你会改变现在的生活方式吗?",
+    "为什么？",
+    "友谊对你来说意味着什么？",
+    "爱和情感在你生活中扮演什么样的角色？",
+    "轮流分享你认为约会对象的好品质。每人说出五条 你的家人之间关系是否亲密而温暖？你觉得自己的童年比大多数人更快乐吗？",
+    "你觉得你和母亲的关系怎么样",
+];
+
+const chineseQuestionsTopic3 = [
+    "每人用“我们”开头说三句真心话。例如，我们都在这个zoom房间约会，感觉很开心…… ?",
+    "用这个句子造句：我希望有人能和我分享…… Bla, bla, bla ?",
+    "如果你要和对方成为好朋友，他最应该知道的事情是什么?请与他分享。",
+    "告诉你的约会对象，你喜欢他什么，一定要诚实，请说些你可能不会对第一次见面的人说的话。",
+    "分享下你人生中的某个尴尬时刻。",
+    "你上一次在别人面前哭是什么时候? 自己独自一人哭泣又是什么时候？",
+    "告诉对方，你已经喜欢上他身上什么品质了？",
+    "你觉得什么事情是不能随便开玩笑的？",
+    "如果你今晚就会死去，而且没有机会和任何人说，你最遗憾的没有说出口的话是什么? 你为什么还没告诉他们？",
+    "你的房子着火了，而你所有的东西都在你的房子里，在你救出你爱的人和宠物之后，你还 有时间安全的做最后冲刺去救任何一件物品。那样东西会是什么呢?为什么",
+    "你的家人中，谁的去世会让你觉得最心神不安?为什么？",
+    "分享一个关于你的私人问题，请你的约会对象给出建议，请他以自己的方式来处理这个问题。并且，让你的约会对象反馈给你，你对所选择的私人问题感觉如何？",
+    "在做最后的约会选择之前，你最后想要问你的约会对象",
+];
+
+export {
+    englishQuestionsTopic1,
+    englishQuestionsTopic2,
+    englishQuestionsTopic3,
+    chineseQuestionsTopic1,
+    chineseQuestionsTopic2,
+    chineseQuestionsTopic3,
+};
